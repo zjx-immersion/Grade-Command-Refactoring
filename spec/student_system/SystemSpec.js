5ab007e5d748b9a6ed15207dd54529a62c720273
gaoyx <melo@gaoyuexiang>
2017-03-29 15:38:53 +0800

    add test case for updateClasses

--- spec/student_system/SystemSpec.js
+++ spec/student_system/SystemSpec.js
@@ -3,13 +3,23 @@ describe('system', function () {
   const Student = require('../../lib/student_system/Student');
   const Subject = require('../../lib/student_system/Subject');
   let system;
+
   beforeEach(function () {
     system = new System();
   });
+
   it('should return student info when given an input string of student', function () {
     const inputStr = 'Melo,24,Han,1,math:90,chinese:80,english:70,program:100';
     const subject = new Subject(90, 80, 70, 100);
     const expectStudent = new Student('Melo', 24, 'Han', 1, subject);
     expect(system.getStudentInfo(inputStr)).toEqual(expectStudent);
+  });
+
+  it('should return updated classes when given a new student', function () {
+    const subject = new Subject(90, 80, 70, 100);
+    const student = new Student('Melo', 24, 'Han', 1, subject);
+    let expectClass = new Class();
+    expectClass.addStudent(student);
+    expect(system.updateClasses(student)).toEqual([expectClass]);
   });
 });
