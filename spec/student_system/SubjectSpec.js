1c3b9bc6675ca495cb2469eb6fb2fdca700a4720
gaoyx <melo@gaoyuexiang>
2017-03-29 17:23:59 +0800

    add test case for subject.calAverage when average is a float and passed

--- spec/student_system/SubjectSpec.js
+++ spec/student_system/SubjectSpec.js
@@ -1,6 +1,9 @@
 describe('subject', function () {
   const Subject = require('../../lib/student_system/Subject');
-  it('should return average when invoke subject.calAverage', function () {
+  it('should return average when average is a integer', function () {
     expect(new Subject(90, 80, 70, 100).calAverage()).toEqual(85);
+  });
+  it('should return average when average is not a integer', function () {
+    expect(new Subject(90, 92, 93, 94).calAverage()).toEqual(92.25);
   });
 });
